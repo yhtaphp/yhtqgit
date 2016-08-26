afe8a2b4b17b1109b204029fb3fe4425f5ab8635
yhtaphp <yhtqduan@yeah.net>
2016-08-27 00:25:45 +0800

    thinkphp

--- thinkphp/Public/js/admin.js
+++ thinkphp/Public/js/admin.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
 	
-	$("#botton").click(function(){
+	$(".btn-img").click(function(){
 		var btn = document.getElementById("btn");
 		btn.click();
+	});
+	$("#btn").change(function(){
+		$(".show").attr("src",window.URL.createObjectURL(document.getElementById("btn").files[0]));
+		alert("文件选择的内容改变了");
 	});
 });
